cdd04d62ad849c5604037659c12754ba9e9b1bc5
Bolan <m.bolan.m@gmail.com>
2024-09-12 12:33:55 +0200

    CombatComponents, HitPointsAuthoring, and TeamAuthoring

---
+++ Assets/Scripts/Common/HitPointsAuthoring.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Entities;
+
+using UnityEngine;
+
+namespace Common
+{
+    public class HitPointsAuthoring : MonoBehaviour
+    {
+        public int maxHitPoints;
+        private class HitPointsAuthoringBaker : Baker<HitPointsAuthoring>
+        {
+            public override void Bake(HitPointsAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(entity, new MaxHitPoints() { Value = authoring.maxHitPoints });
+                AddComponent(entity, new CurrentHitPoints { Value = authoring.maxHitPoints });
+                AddBuffer<DamageBufferElement>(entity);
+                AddBuffer<DamageThisTick>(entity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Common/MobaTeamAuthoring.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+
+using UnityEngine;
+
+namespace Common
+{
+    public class MobaTeamAuthoring : MonoBehaviour
+    {
+        public TeamType team;
+        private class MobaTeamAuthoringBaker : Baker<MobaTeamAuthoring>
+        {
+            public override void Bake(MobaTeamAuthoring authoring)
+            {
+                var e = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent(e, new MobaTeam { Value = authoring.team} );
+            }
+        }
+    }
+}
